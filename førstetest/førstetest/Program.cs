03c9475e7c448da2bc0cdf22f3f65c9eebc9611b
StormyV2 <Stormyyv77@gmail.com>
2024-05-21 13:04:36 +0200

    gjort ferdig

---
+++ førstetest/førstetest/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hei på deg! Hva heter du?");
+var navn = Console.ReadLine();
+Console.WriteLine($"Hei! {navn} hvordan har dagen din vært?");
+var dagen = Console.ReadLine();
+Console.WriteLine("Høres ut som en bra dag!");
